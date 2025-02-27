7f92c36e7aa0cdbc0c38d309c3d64b71880a2b0f
Damirka-steck <fast.gepard.08.07@gmail.com>
2025-02-27 16:47:16 +0500

    Update logic.ts

--- src/logic.ts
+++ src/logic.ts
@@ -6,8 +6,14 @@ type Product = {
 };
  
 function filterAndSortProducts(products: Product[]): Product[] {
-    // Your code goes here
-    return [] 
+    const uniqueProduct = new Map<string, Product>();
+
+        for(const prduct of products){
+            if (!uniqueProducts.has(product.name)){
+                uniqueProducts.set(product.name, product);
+            }
+        }
+    return [Array.from(uniqueProduct.values()).sort((a,b)=> a.price - b.price)] 
 }
 
 module.exports = { filterAndSortProducts }
